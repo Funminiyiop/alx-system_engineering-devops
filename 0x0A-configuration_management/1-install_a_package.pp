a183c8032751a0c89004bf33fcd44bbb21b07f57
Funminiyi Peter <funminiyiop@gmail.com>
2024-02-28 10:35:34 +0100

    a new 1-install_a_package.pp file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,4 @@
-#!/usr/bin/pup
 # install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
+exec { 'flask':
+  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
 }
-
