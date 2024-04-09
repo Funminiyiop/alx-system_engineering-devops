b35d2b922ee47905775c60f0ab884e2ad999a941
Funminiyi Peter <funminiyiop@gmail.com>
2024-04-09 16:20:53 +0100

    adding modified 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,13 @@ def number_of_subscribers(subreddit):
     Returns:
         number of subscribers to the subreddit,
         or 0 if subreddit requested is invalid"""
-    headers = {'User-Agent': 'xica369'}
+    headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
+    if response.status_code == 404:
+        return 0
     if response.status_code == 200:
         return (response.json().get("data").get("subscribers"))
     return (0)
+
